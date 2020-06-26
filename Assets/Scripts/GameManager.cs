736599e341b42b11a6f553fc478a2ce168b64987
magajanes <thiagosmfis@gmail.com>
2020-06-26 20:29:42 -0300

    Set target frame rate to 60 fps

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = -1;
+        Application.targetFrameRate = 60;
         QualitySettings.vSyncCount = 0;
 
         if (Instance != null)
