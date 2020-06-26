56a783d545737d74a58310385a0d102ed391cf99
magajanes <thiagosmfis@gmail.com>
2020-06-26 18:54:50 -0300

    Implement camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("Target parameters")]
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private Transform target;
 
+    [Header("Lerp parameters")]
+    [SerializeField]
+    private float lerpTime;
+
+    private Vector3 _position;
+
+    private void Start()
+    {
+        transform.position = target.position + offset;
+    }
+
+    private void FixedUpdate()
+    {
+        _position = transform.position;
+        _position.x = Mathf.Lerp(_position.x, target.position.x + offset.x, lerpTime * Time.deltaTime);
+        transform.position = _position;
+    }
 }
